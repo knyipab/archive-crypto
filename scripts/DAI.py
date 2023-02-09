df43f1650bf43116a99e98b80e3a364d7fdf9027
Ronald Y <46513942+knyipab@users.noreply.github.com>
2023-02-09 01:50:53 +0000

    DAI.py action failed fix

--- scripts/DAI.py
+++ scripts/DAI.py
@@ -12,7 +12,7 @@ def archive(browser, wa, folder):
     browser.archive_mhtml('{}/{}-{}.mhtml'.format(folder, name, time_str))
     collateral_table = pd.DataFrame(json.loads(wa.summarize_response(responses[0])['response'])['data']['collaterals'])
     try:
-        for pathname in collateral_table['COLLATERAL'].str.extract('href="([^"]*)"')[0].to_list():
-            responses += browser.retry_until_no_request(pathname, '/data/', timeout=10, timer=3)
+        #for pathname in collateral_table['COLLATERAL'].str.extract('href="([^"]*)"')[0].to_list():
+        #    responses += browser.retry_until_no_request(pathname, '/data/', timeout=10, timer=3)
     finally:
         wa.archive_response(responses, '{}/{}-{}.json'.format(folder, name, time_str))
