2c2f17d768678ed0b000bfe61a79b592a0c50c27
Ronald Y <46513942+knyipab@users.noreply.github.com>
2023-02-10 14:51:29 +0800

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+sudo apt update
+sudo apt upgrade -y
+# curl -s -L https://raw.githubusercontent.com/microsoft/vscode-dev-containers/main/script-library/desktop-lite-debian.sh | sudo bash
+
 pip install -r requirements.txt
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
 rm google-chrome-stable_current_amd64.deb
 version=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget "https://chromedriver.storage.googleapis.com/$version/chromedriver_linux64.zip"
